f32d6c29d1bc1dba33bc61449b25a978a3b1c54f
Luís Bastião Silva <bastiao@bmd-software.com>
2021-05-09 12:42:38 +0100

    Added documents pha edit

--- documents/documents.go
+++ documents/documents.go
@@ -2,6 +2,11 @@ package documents
 
 import (
 	"log"
+	"gopkg.in/yaml.v2"
+    "fmt"
+    "bytes"
+	"os"
+	"bufio"
 
 	"github.com/uber/gonduit"
 	"github.com/uber/gonduit/requests"
@@ -10,11 +15,14 @@ import (
 type WikiSlugRequest struct {
 	Slug    string   `json:"slug"`
 	Content    string   `json:"content"`
+	requests.Request                    // Includes __conduit__ field needed for authentication.
+
 }
 
 
 type ConstraintsRequest struct {
 	Statuses []string `json:"statuses"`
+	Paths []string `json:"paths"`
 	Query    string   `json:"query"`
 }
 
@@ -81,9 +89,34 @@ func LookForDocument(client *gonduit.Conn, documentQuery *string) PhidDocumentDa
 }
 
 
-// Edit Document
-func EditDoc(client *gonduit.Conn, documentQuery *string, content *string, slug *string) WikiPhidDocumentResponse {
+func LookForDocumentByPath(client *gonduit.Conn, documentPath *string) PhidDocumentDataResponse {
+	conActive := "active"
 
+	constraints := &ConstraintsRequest{
+		Statuses: []string{conActive},
+		Paths: []string{*documentPath},
+	}
+	attachments := &AttachmentsRequest{
+		Content: true,
+	}
+
+	req := &PhidDocumentRequest{
+		Constraints: *constraints,
+		Attachments: *attachments,
+	}
+	var res PhidDocumentDataResponse
+
+	err1 := client.Call("phriction.document.search", req, &res)
+	if err1 != nil {
+		log.Fatal("Error: ", err1)
+	}
+	return res
+}
+
+// Edit Document
+func EditDoc(client *gonduit.Conn, content *string, slug *string) WikiPhidDocumentResponse {
+	fmt.Println("📃 EditDoc content: ", *content)
+	fmt.Println("📃 EditDoc slug: ", *slug)
 	req := &WikiSlugRequest{
 		Slug:    *slug,
 		Content: *content,
@@ -96,4 +129,68 @@ func EditDoc(client *gonduit.Conn, documentQuery *string, content *string, slug 
 		log.Fatal("Error: ", err1)
 	}
 	return res
+}
+
+func ReadTemplate(pathTemplate string) string {
+
+    f, err := os.Open(pathTemplate)
+
+    if err != nil {
+        fmt.Println(err)
+     }
+
+    defer f.Close()
+
+    scanner := bufio.NewScanner(f)
+	var content string 
+	content = ""
+    for scanner.Scan() {
+
+        content = content + scanner.Text() + "\n"
+		
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Println(err)
+    }
+	return content
+
+}
+
+type Customer struct {
+    Slug string `yaml:"slug"`
+    Domain string `yaml:"domain"`
+    Team string `yaml:"team"`
+	Name string 
+}
+
+
+type Customers map[string]*Customer
+func (s *Customers) Unmarshal(data []byte) error {
+    err := yaml.NewDecoder(bytes.NewReader(data)).Decode(s)
+    if err != nil {
+        return err
+    }
+    for k, v := range *s {
+        v.Name= k
+    }
+    return nil
+}
+
+// Load Mappings
+func LoadMappings() Customers {
+    var yamlFile string
+	yamlFile = ReadTemplate("support/mapping.yaml")
+
+    var customers Customers = map[string]*Customer{}
+    err :=customers.Unmarshal([]byte(yamlFile))
+    if err != nil {
+        log.Fatal("failed to decode: %v", err)
+    }
+
+    /*for k, v := range customers {
+        fmt.Printf("%v -> name: %v, \n", k, v.Slug, v.Team)
+    }*/
+	return customers
+
 }
